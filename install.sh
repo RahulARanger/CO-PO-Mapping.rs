55fca375dbfc6af92bf17f5a11f958597169e01d
Rahul <saihanumarahul66@gmail.com>
2024-07-04 01:20:03 +0530

    fix: installation script for rust referred: https://github.com/vercel-community/rust/blob/main/src/lib/rust-toolchain.ts

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-curl https://sh.rustup.rs -sSf | sh -s -- -y;
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+rustup -V
 . "$HOME/.cargo/env";
 pnpm install;
 npm install -g wasm-pack;
