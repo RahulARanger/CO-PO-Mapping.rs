a37e314f2f3498fa568dc79cde68d48859eb837c
Rahul <saihanumarahul66@gmail.com>
2024-07-04 02:00:58 +0530

    fix: install cargo dependencies

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 # this is the build script that would run past the installation script in vercel
 
+# install packages for worker-crate and builds target files
+cargo build
+
+ls
+
 # prepare crate
 pnpm build-crate
 
-ls
 ls pkg
+
 pnpm build
--- install.sh
+++ install.sh
@@ -3,10 +3,8 @@
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
-pnpm install;
 
 # wasm-pack is required to compile our rust to wasm
 npm install -g wasm-pack;
 
-# install packages for worker-crate
-cargo install
+pnpm install;
