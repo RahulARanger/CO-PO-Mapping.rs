9001cfbaaf6f8837438e5948932e2cc059444f01
Rahul <saihanumarahul66@gmail.com>
2024-07-04 02:20:17 +0530

    fix: build cargo crate

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 
+# c-make was required to install wasm-pack
+apt-get update
+apt-get install -y cmake
+
 # wasm-pack is required to compile our rust to wasm
 cargo install wasm-pack
 
