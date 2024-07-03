ee778cb9667e797c305f9a4ab748615c075dcca4
Rahul <saihanumarahul66@gmail.com>
2024-07-04 02:02:54 +0530

    fix: install cargo dependencies

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 # this is the build script that would run past the installation script in vercel
+source "$HOME/.cargo/env"
 
 # install packages for worker-crate and builds target files
 cargo build
